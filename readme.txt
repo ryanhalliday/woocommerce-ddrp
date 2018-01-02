19e07404a351111655474ca27670d4eee29de948
Ryan Halliday <ryan@hellodigital.nz>
2018-01-02 16:53:15 +1300

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Disable Downloadable Repeat Purchase - WooCommerce ===
+Contributors: squareonemedia, rwebster85
+Author URI: https://www.squareonemedia.co.uk
+Plugin URL: https://wordpress.org/plugins/disable-downloadable-repeat-purchase-woocommerce/
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VAYF6G99MCMHU
+Requires at Least: 3.7
+Tested Up To: 4.5.2
+Tags: woocommerce, downloads, downloadable, repeat purchase, wordpress, wordpress.org
+
+For WooCommerce. Disable the ability for logged in users to purchase items they already own that are downloadable.
+
+
+== Description ==
+
+This WooCommerce plugin prevents a user from being able to purchase a downloadable product that they already own, as long as they are eligable to download that product. In place of the "Add to Basket" button on the product page, a message will display informing the user they already own the item, and links to download the linked files are provided there. Those links are the special links to the file that are provided on the "My Downloads" page, or part of the "My Account" page.
+
+If the user has had a refund for the item, or if they have used up all of their allowed download (when product has a limited number of downloads) then the normal "Add to Basket" button will show again, allowing purchase.
+
+On any page that displays the products other than the single product page, the button shows as "Read More", as though the product is not available.
+
+
+
+== Installation ==
+
+Just install the plugin as normal and activate it. Nothing else required.
+
+
+
+== Frequently Asked Questions ==
+
+= How does it work? =
+
+The plugin file adds 2 functions that hook into WooCommerce using add_filter on 'woocommerce_is_purchasable', and 'woocommerce_single_product_summary'.
+
+It uses div tags, including <div class="woocommerce-info wc-nonpurchasable-message">, which means it should be styled in conjunction with any WooCommerce theme. Tested on Storefront.
+
+
+== Screenshots ==
+
+1. The disabled message with default WooCommerce styles
+2. How it appears with Storefront theme
+
+
+== Changelog ==
+= 1.0 = 
+* Initial release
---
+++ woocommerce-disable-downloadable-repeat-purchase.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Disable Downloadable Repeat Purchase - WooCommerce
+Description: Disable the ability for logged in users to purchase items they already own that are downloadable. Links are provided on the product page for ease of re-download. It does not apply to products the customer has been refunded for, or has their download expired. It checks if the product has been purchased and is available to download on their account.
+Author: Square One Media
+Author URI: https://www.squareonemedia.co.uk
+Version: 1.0
+Text Domain: disable-downloadable-repeat-purchase
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+function som_disable_repeat_purchase( $purchasable, $product ) {
+
+	// Get the ID for the current product
+	$product_id = $product->id; 
+ 
+	// return false if the customer has bought the product and is currently available for download
+	if ( wc_customer_bought_product( get_current_user()->user_email, get_current_user_id(), $product_id ) && ($product->downloadable == 'yes') ) {
+
+		if ( $downloads = WC()->customer->get_downloadable_products() ) {
+
+				foreach ( $downloads as $download ) :
+					if ($download['product_id'] == $product->id) {
+						$purchasable = false;
+					}
+				endforeach;
+
+		}
+	}
+
+	return $purchasable;
+}
+add_filter( 'woocommerce_is_purchasable', 'som_disable_repeat_purchase', 10, 2 );
+
+function som_repeat_purchase_disabled_message() {
+
+	global $product;
+	$once = 0;
+	$end = false;
+
+	if ( wc_customer_bought_product( get_current_user()->user_email, get_current_user_id(), $product->id ) && ($product->downloadable == 'yes') ) {
+
+		if ( $downloads = WC()->customer->get_downloadable_products() ) {
+
+			do_action( 'woocommerce_before_available_downloads' ); ?>
+
+				<?php foreach ( $downloads as $download ) :
+
+					if ($download['product_id'] == $product->id) {
+
+						$once++;
+
+						if ($end == false)
+							$end = true;
+
+						if ($once == 1) {
+							echo '<div class="woocommerce"><div class="woocommerce-info wc-nonpurchasable-message"><p><strong>You\'ve already purchased this product.</strong><br>Download links below.</p>';
+							echo '<div class="product-page-links">';
+
+							//The below codes injects some CSS for smaller than desktop devices. Depending on how many download links there are, the list can get quite long, which may affect the site styling depending on the theme. Specifically a default custom theme. Remove the below echo line if this is not required.
+							echo '<style>@media (max-width : 1200px){ .summary.entry-summary {width: 100%!important; display: block;} }</style>';
+
+						}
+
+						do_action( 'woocommerce_available_download_start', $download );
+
+						if ( is_numeric( $download['downloads_remaining'] ) ) {
+							echo apply_filters( 'woocommerce_available_download_count', '<span class="count">' . sprintf( _n( '%s download remaining', '%s downloads remaining', $download['downloads_remaining'], 'woocommerce' ), $download['downloads_remaining'] ) . '</span> ', $download );
+						}
+
+						echo apply_filters( 'woocommerce_available_download_link', '<a class="download-link-product-page" href="' . esc_url( $download['download_url'] ) . '">' . $download['download_name'] . '</a>', $download ) ;
+
+						do_action( 'woocommerce_available_download_end', $download );
+
+					}
+
+				endforeach;
+
+					if ($end) {
+						echo '</div></div></div>';
+					}
+
+			do_action( 'woocommerce_after_available_downloads' );
+
+			}
+
+		}
+
+}
+add_action( 'woocommerce_single_product_summary', 'som_repeat_purchase_disabled_message', 31 );
